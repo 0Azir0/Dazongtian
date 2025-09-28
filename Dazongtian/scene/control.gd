5800ad018b48925e002af28a9f4444ddaa8b61d3
ying0407OuOb <mikechen20090407@gmail.com>
2025-09-28 14:13:31 +0800

    ouo

--- Dazongtian/scene/control.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("esc"):
-		get_tree().change_scene_to_file("res://scene/world_1.tscn")
-
-func _on_video_stream_player_finished() -> void:
-	get_tree().change_scene_to_file("res://scene/world_1.tscn")
-	pass # Replace with function body.
--- Dazongtian/script/PlayerAttribute.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-var saved_velocity: Vector2 = Vector2.ZERO
-var saved_global_position_x: float = 0.0
-var saved_global_position_y: float = 0.0
